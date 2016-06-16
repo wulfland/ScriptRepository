657c287f5eed0317ebe2ad2600204519a9ef5669
admine63755 Kaufmann (Alegri) <michael_kaufmann@hugoboss.com>
2016-06-16 13:42:30 +0200

    Added scripts to list all releases

---
+++ DevOps/DevOps/Get-VSTSRelease.ps1
@@ -0,0 +1,41 @@
+﻿[CmdletBinding()]
+Param (
+    [Parameter(Mandatory=$true, Position=0)]
+    $Account,
+
+    [Parameter(Mandatory=$true, Position=1)]
+    $Accesstoken
+)
+
+$passkey = ":$($Accesstoken)"
+$encodedKey = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($passkey))
+$token = "Basic $encodedKey"
+
+$projectsResult = Invoke-RestMethod "https://$Account.visualstudio.com/_apis/projects?api-version=1.0" -Method Get  -Headers @{ Authorization = $token }
+
+$releases = @()
+
+$projectsResult.value | % {
+
+    $project = $_.name
+
+    $url = "https://$Account.vsrm.visualstudio.com/$project/_apis/release/releases?api-version=3.0-preview.2"
+
+    $result = Invoke-RestMethod $url -Method Get -Headers @{ Authorization = $token }
+
+    if ($result.count -gt 0){
+
+        $result.value | % {
+            
+            $releases += [pscustomobject]@{ 
+                Project = $project 
+                Name = $_.Name 
+                Status = $_.status
+                CreatedOn = [datetime]$_.createdOn
+                ModifiedOn = [datetime]$_.modifiedOn 
+            }
+        }
+    }
+}
+
+$releases | Sort-Object -Property ModifiedOn -Descending | Format-Table -AutoSize
