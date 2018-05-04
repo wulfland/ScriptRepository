7cbbb2b007dc759bf8444912f1a25e83ef1b87b1
Michael Kaufmann <kaufmannm@outlook.de>
2018-05-04 09:21:51 +0200

    Added script to set repo permision

---
+++ Set-RepositoryPermission/Readme.md
@@ -0,0 +1,65 @@
+﻿# NAME
+Set-RepositoryPermission.ps1
+    
+# SYNOPSIS
+Set the permissions for a git repository to enforce naming policies
+     
+# SYNTAX
+``` PowerShell
+.\Set-RepositoryPermission.ps1 [-AccountOrCollection] <String> [-ProjectName] <String> [-Repository] <String> [[-AllowedFolders] <String[]>] [[-AdminAllowedFolders] <String[]>] [[-PathToTfExe] <String>] [-WhatIf] [-Confirm] [<CommonParameters>]
+``` 
+    
+# DESCRIPTION
+This script sets the permission of a git repository. It denys to create branches in the >>root and only allows the creation of branches in specific folders.
+    
+# PARAMETERS
+    -AccountOrCollection <String>
+        Your VSTS account or TFS project collection url
+        
+    -ProjectName <String>
+        The name of the project that contains your repository
+        
+    -Repository <String>
+        The name of the repository
+        
+    -AllowedFolders <String[]>
+        The list of folders where contributors can create branches in (default: features, bufixes, users)
+        
+    -AdminAllowedFolders <String[]>
+        The list of folders where contributors can create branches in (default: features, bufixes, users, releases, master)
+        
+    -PathToTfExe <String>
+        The path where tf.exe is installed (default: Visual Studio 2017 location 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer').
+        
+    -WhatIf [<SwitchParameter>]
+        
+    -Confirm [<SwitchParameter>]
+        
+    <CommonParameters>
+        This cmdlet supports the common parameters: Verbose, Debug,
+        ErrorAction, ErrorVariable, WarningAction, WarningVariable,
+        OutBuffer, PipelineVariable, and OutVariable. For more information, see 
+        about_CommonParameters (http://go.microsoft.com/fwlink/?LinkID=113216). 
+    
+# Examples
+
+## -------------------------- EXAMPLE 1 --------------------------
+``` PowerShell
+C:\PS>Set-RepositoryPermission.ps1 -AccountOrCollection https://tfs.contoso.com/tfs/DefaultCollection -ProjectName MyProject -Repository MyProject -Verbose
+ ```   
+
+## -------------------------- EXAMPLE 2 --------------------------
+ ``` PowerShell 
+C:\PS>Set-RepositoryPermission.ps1 -AccountOrCollection https://constoso.visualstudio.com -ProjectName MyProject -Repository MyProject -Verbose
+```    
+   
+## -------------------------- EXAMPLE 3 --------------------------
+``` PowerShell    
+C:\PS>Set-RepositoryPermission.ps1 -AccountOrCollection https://constoso.visualstudio.com -ProjectName MyProject -Repository MyProject -AllowedFolders @("features", "users")
+```    
+    
+    
+# REMARKS
+ To see the examples, type: "get-help .\Set-RepositoryPermission.ps1 -examples".  
+ For more information, type: "get-help .\Set-RepositoryPermission.ps1 -detailed".  
+ For technical information, type: "get-help .\Set-RepositoryPermission.ps1 -full".
---
+++ Set-RepositoryPermission/Set-RepositoryPermission.ps1
@@ -0,0 +1,92 @@
+﻿<#
+.Synopsis
+   Set the permissions for a git repository to enforce naming policies
+.DESCRIPTION
+   This script sets the permission of a git repository. It denys to create branches in the root and only allows the creation of branches in specific folders.
+.EXAMPLE
+   Set-RepositoryPermission.ps1 -AccountOrCollection https://tfs.contoso.com/tfs/DefaultCollection -ProjectName MyProject -Repository MyProject -Verbose 
+.EXAMPLE
+   Set-RepositoryPermission.ps1 -AccountOrCollection https://constoso.visualstudio.com -ProjectName MyProject -Repository MyProject -Verbose 
+.EXAMPLE
+   Set-RepositoryPermission.ps1 -AccountOrCollection https://constoso.visualstudio.com -ProjectName MyProject -Repository MyProject -AllowedFolders @("features", "users")
+#>
+[CmdletBinding(SupportsShouldProcess=$true)]
+Param
+(
+    # Your VSTS account or TFS project collection url
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidatePattern("http://.*|https://.*")]
+    [string]
+    $AccountOrCollection,
+
+    # The name of the project that contains your repository
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $ProjectName,
+
+    # The name of the repository
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $Repository,
+
+    # The list of folders where contributors can create branches in (default: features, bufixes, users)
+    [Parameter(Mandatory=$false)]
+    [string[]]
+    $AllowedFolders = @("features", "bugfixes", "users"),
+
+    # The list of folders where contributors can create branches in (default: features, bufixes, users, releases, master)
+    [Parameter(Mandatory=$false)]
+    [string[]]
+    $AdminAllowedFolders = @("features", "bugfixes", "users", "releases", "master"),
+
+    # The path where tf.exe is installed (default: Visual Studio 2017 location 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer').
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({ Test-Path -PathType Container $_ })]
+    [string]
+    $PathToTfExe = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer"
+)
+
+pushd $PathToTfExe
+
+
+# block the Create Branch permission at the repository root for the project's contributors
+Write-Verbose "Deny the 'CreateBranch' permission to the group '[$ProjectName]\Contributors' in repository '$Repository' ($AccountOrCollection)."
+if ($pscmdlet.ShouldProcess("$Repository", "Deny CreateBranch"))
+{
+    & .\tf.exe git permission /deny:CreateBranch /group:[$ProjectName]\Contributors /collection:$AccountOrCollection /teamproject:$ProjectName /repository:$Repository
+}
+
+# block the Create Branch permission at the repository root for the project's admins
+Write-Verbose "Deny the 'CreateBranch' permission to the group '[$ProjectName]\Project Administrators' in repository '$Repository' ($AccountOrCollection)."
+if ($pscmdlet.ShouldProcess("$Repository", "Deny CreateBranch"))
+{
+    & .\tf.exe git permission /deny:CreateBranch /group:"[$ProjectName]\Project Administrators" /collection:$AccountOrCollection /teamproject:$ProjectName /repository:$Repository
+}
+
+# Allow contributors to create branches under configured folders
+$AllowedFolders | ForEach-Object {
+    
+    Write-Verbose "Allow the 'CreateBranch' permission to the group '[$ProjectName]\Contributors' for folder '$_' in repository '$Repository' ($AccountOrCollection)."
+    if ($pscmdlet.ShouldProcess("$Repository/$_", "Allow CreateBranch"))
+    {
+        & .\tf.exe git permission /allow:CreateBranch /group:[$ProjectName]\Contributors /collection:$AccountOrCollection /teamproject:$ProjectName /repository:$Repository /branch:$_
+    }
+}
+
+# Allow admins to create branches under configured folders
+$AdminAllowedFolders | ForEach-Object {
+    
+    Write-Verbose "Allow the 'CreateBranch' permission to the group '[$ProjectName]\Project Administrators' for folder '$_' in repository '$Repository' ($AccountOrCollection)."
+    if ($pscmdlet.ShouldProcess("$Repository/$_", "Allow CreateBranch"))
+    {
+        & .\tf.exe git permission /allow:CreateBranch /group:"[$ProjectName]\Project Administrators" /collection:$AccountOrCollection /teamproject:$ProjectName /repository:$Repository /branch:$_
+    }
+}
+
+popd 
+
+
